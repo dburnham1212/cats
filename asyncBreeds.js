34ee76032d9bc3073fcd0c3b15114a0964c93efb
dburnham1212 <dburnham1212@gmail.com>
2023-06-06 22:53:40 -0400

    created unit testing for asyncBreeds.js and syncBreeds.js using Mocha and Chai

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -5,14 +5,13 @@ const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // CHANGE: Pass data into callback instead of returning it directly
     console.log("In readFile's Callback: it has the data.");
-    if (!error) functionToRunWhenThingsAreDone(data);
+    if (!error) {
+      functionToRunWhenThingsAreDone(data);
+    }
+    else {
+      functionToRunWhenThingsAreDone(undefined);
+    }
   });
 };
 
-// CHANGE 1: Moved the console.log into a new function:
-const printOutCatBreed = breed => {
-  console.log('Return Value: ', breed) // => print out details correctly.
-};
-
-// CHANGE 2: we're now passing two arguments into breedDetailsFromFile: breed string and a callback function
-breedDetailsFromFile('Bombay', printOutCatBreed);
+module.exports = breedDetailsFromFile;
